e223e8208cb12437c820b11de945b261d32b4a0c
Charles Cusson <charlescusson@gmail.com>
2020-03-12 02:25:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 function makeInt(string){
 
-return parseInt(string, 10)
+return parseInt('string', 10)
 }
 
 function preserveDecimal(string){
-  return parseFloat(string)
+  return parseFloat('string')
 }
